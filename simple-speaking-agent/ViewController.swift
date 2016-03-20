b5a5631bb2f17070ba8ba236658660f7746c6c92
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2016-03-21 02:51:04 +0900

    Initial Commit

---
+++ simple-speaking-agent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-speaking-agent
+//
+//  Created by うえでー on 2016/03/21.
+//  Copyright © 2016年 Daiki UEDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
